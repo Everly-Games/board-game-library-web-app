dc8de899b85d20b035a49f6d13e31d139ac5d6d6
Nate Baker <18668649+nathantbaker@users.noreply.github.com>
2025-10-30 13:27:41 -0500

    upload dewey assets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,8 @@
   <title>Board Game Library</title>
   <meta name="description" content="Find the perfect board game for any vibe.">
 </svelte:head>
-<div class="bg-white min-h-screen flex flex-col items-center justify-center text-center">
+<div class="bg-white min-h-screen flex flex-col items-center justify-center text-center transform -translate-y-[10%]">
+  <img src="/images/dewey_on_books.svg" class="h-35" />
   <h1 class="text-4xl font-bold text-gray-900 mb-2">
     Board Game Library
   </h1>
