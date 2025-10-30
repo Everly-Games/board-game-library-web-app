b9ec08a75e90b93822c6f83cf98540ff3f4d8871
Nate Baker <18668649+nathantbaker@users.noreply.github.com>
2025-10-30 11:11:42 -0500

    home page title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+  <title>Board Game Library</title>
+  <meta name="description" content="Find the perfect board game for any vibe.">
+</svelte:head>
 <div class="bg-white min-h-screen flex flex-col items-center justify-center text-center">
   <h1 class="text-4xl font-bold text-gray-900 mb-2">
     Board Game Library
