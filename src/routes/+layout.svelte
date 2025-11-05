23f6bd5a1f44648dea1617424479e3d02656305e
Nate Baker <18668649+nathantbaker@users.noreply.github.com>
2025-11-05 09:37:50 -0600

    move to dewey logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,11 +6,9 @@
 </script>
 
 <svelte:head>
-  <link rel="icon" type="image/svg+xml" href="/favicon-light.svg" media="(prefers-color-scheme: light)" />
-  <link rel="icon" type="image/svg+xml" href="/favicon-dark.svg" media="(prefers-color-scheme: dark)" />
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
   <!-- safari -->
-  <link rel="icon" type="image/png" href="/favicon-light.png" media="(prefers-color-scheme: light)">
-  <link rel="icon" type="image/png" href="/favicon-dark.png" media="(prefers-color-scheme: dark)">
+  <link rel="icon" type="image/png" href="/favicon.png" />
   <meta name="theme-color" content="#A8A397" />
 </svelte:head>
 
